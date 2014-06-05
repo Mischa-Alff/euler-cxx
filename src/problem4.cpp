6405f0286fa97ccbfca228a91c311551892bf0aa
Mischa Aster Alff <mischa@destrock.com>
2014-06-05 15:19:58 +0200

    Added Problem 4

---
+++ src/problem4.cpp
@@ -0,0 +1,25 @@
+#include <list>
+#include <cstdlib>
+#include <iostream>
+
+bool is_palindrome(int n)
+{
+	auto s = std::to_string(n);
+	size_t len = s.length();
+	for(int i=0;i<len/2;++i)
+		if(s[i]!=s[len-i-1])
+			return false;
+	return true;
+}
+
+int main()
+{
+	std::list<int> palindromes;
+	int solution{0};
+	for(int x{999};x>=100&&!solution;--x)
+		for(int y{x};y>=100&&!solution;--y)
+			if(is_palindrome(x*y))
+				palindromes.push_back(x*y);
+	palindromes.sort();
+	std::cout<<palindromes.back()<<std::endl;
+}
