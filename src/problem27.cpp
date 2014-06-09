e82449b7c50b3163c4b52e41630a753e41670816
Mischa Aster Alff <mischa@destrock.com>
2014-06-09 02:37:30 +0200

    More optimizations

--- src/problem27.cpp
+++ src/problem27.cpp
@@ -20,9 +20,9 @@ int main()
 	eratosthenes(972, primes);
 	int16_t num_primes{0}, sa, sb, n;
 
-	for(int16_t a{-999};a<1000;a+=2)
+	for(int16_t a{-999};a<999;a+=2)
 	{
-		for(int16_t b{3};b<998;b+=2)
+		for(int16_t b=(a>3)?a:3;b<998;b+=2)
 		{
 			if(!primes[b])
 				continue;
