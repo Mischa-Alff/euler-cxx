48805250ab9a887d07403aa435a2719c8677ead4
Mischa Aster Alff <mischa@destrock.com>
2014-06-10 23:37:23 +0200

    Optimized Problem 10

--- src/problem10.cpp
+++ src/problem10.cpp
@@ -1,29 +1,37 @@
+#include <cmath>
 #include <vector>
 #include <iostream>
 
+void eratosthenes(int max, std::vector<bool> &val)
+{
+	val.resize(max, true);
+	val[0]=val[1]=false;
+	int n=sqrt(val.size());
+	for(int i{2};i<=n;++i)
+		if(val[i])
+			for(int j=i*i-i;j<val.size();j+=i)
+			{
+				//std::cout<<j<<" is not prime\n";
+				val[j]=false;
+			}
+	val[2]=true;
+}
+
+bool is_prime(uint64_t n)
+{
+	for(int i=2;i<n;++i)
+		if(!(n%i))
+			return false;
+	return true;
+}
+
 int main()
 {
-	std::vector<uint64_t> primes{2,3,5,7};
-	uint64_t n = primes.back();
-	uint64_t solution = 0;
-	bool prime;
-	while(primes.back() < 2e6)
-	{
-		prime=true;
-		for(auto j : primes)
-		{
-			if(!(n%j))
-			{
-				prime=false;
-				break;
-			}
-		}
-		if(prime)
-			primes.push_back(n);
-		n+=2;
-	}
-	for(auto i:primes)
-		if(i<2e6)
+	std::vector<bool> primes;
+	eratosthenes(2000000, primes);
+	uint64_t solution{0};
+	for(int i=0;i<primes.size();++i)
+		if(primes[i])
 			solution+=i;
 	std::cout<<solution<<std::endl;
 }
